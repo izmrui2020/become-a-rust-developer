2fe260348db1324e6f73397014c988d6be79d444
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-11-11 20:46:30 +0900

    save add filies

--- todo-app/src/main.rs
+++ todo-app/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+
+use actix_web::{web, get, post, Error, App, HttpResponse, HttpServer, Responder};
+
+async fn index() -> Result<Responder<()>, Error> {
+
+    Ok(HttpResponse::Ok()
+        .body("hello world")
+        .expect("aaaaaaa")
+    )
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Listning on port 8080");
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
