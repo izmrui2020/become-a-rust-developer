831b661a95c4f3474949cae3ba859bf94e6f6fe9
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-11-12 22:59:25 +0900

    ok database

--- todo-api/migrations/2021-11-12-132026_create_todos/up.sql
+++ todo-api/migrations/2021-11-12-132026_create_todos/up.sql
@@ -1,11 +1,11 @@
 -- Your SQL goes here
 CREATE TABLE todos (
-    id          INTEGER NOT NULL PRIMARY KEY,
-    kind        VARCHAR NOT NULL PRIMARY KEY,
-    contents    VARCHAR NOT NULL PRIMARY KEY,
-    done        BOOLEAN NOT NULL DEFAULT 0
+    id          SERIAL PRIMARY KEY,
+    kind        VARCHAR NOT NULL,
+    contents    VARCHAR NOT NULL,
+    done        BOOLEAN NOT NULL DEFAULT false
 );
 
-INSERT INTO todos(kind, contents, done) VALUES("private", "go to shop", false);
-INSERT INTO todos(kind, contents, done) VALUES("work", "write code by rust", false);
-INSERT INTO todos(kind, contents, done) VALUES("private", "hogehoge", true);
+-- INSERT INTO todos (kind, contents, done) VALUES ("private", "go to shop", false);
+-- INSERT INTO todos (kind, contents, done) VALUES ("work", "write code by rust", false);
+-- INSERT INTO todos (kind, contents, done) VALUES ("private", "hogehoge", true);
---
+++ todo-api/src/schema.rs
@@ -0,0 +1,8 @@
+table! {
+    todos (id) {
+        id -> Int4,
+        kind -> Varchar,
+        contents -> Varchar,
+        done -> Bool,
+    }
+}
